7e85bd1426a401f10aba861bdde9cd0e75bb2a4a
Varun Ved <varunmved@gmail.com>
2016-03-05 17:31:03 -0800

    oh shit hella webapp tho'

--- index.py
+++ index.py
@@ -1,15 +1,39 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request, redirect
+from flask.ext.wtf import Form
 import flask.ext.login as flask_login
-import flask
+from wtforms import TextField
+from wtforms.validators import Required, Length, ValidationError
 import os
 import csv
 import pprint
 
 app = Flask(__name__)
 
+class LoginForm(Form):
+    fname = TextField('fname')
+    lname = TextField('lname')
+
+def blah(name):
+    return str('hame')
+
 @app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    form = LoginForm(request.form, csrf_enabled = False)
+    if form.validate_on_submit():
+        return blah(form.data['fname'])
+    return render_template('index.html', title = "afd", form = form)
+
+'''
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    email = request.form['email']
+    print("The email address is '" + email + "'")
+    return redirect('/')
+'''
 
 if __name__ == '__main__':
     app.debug = True
